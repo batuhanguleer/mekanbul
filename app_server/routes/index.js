38533eb8157f59c4f22570e8a0e7dbdffefb65f3
batuhanguleer <batuhann.guler@hotmail.com>
2022-10-13 19:27:52 +0300

    last change

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain=require('../controllers/main');
-/* GET home page. */
-router.get('/',ctrlMain.index);
 
+const anaSayfa = function (req, res, next) {
+  res.render('anasayfa', { 'title': 'Ana sayfa' });
+}
+
+const mekanBilgisi = function (req, res, next) {
+  res.render('mekanbilgisi', { 'title': 'Mekan bilgisi' });
+}
+
+const yorumEkle = function (req, res, next) {
+  res.render('yorumekle', { 'title': 'Yorum ekle' });
+}
+
+
+module.exports = {
+
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+
+}
+var express = require('express');
+var router = express.Router();
+var ctrlMekanlar= require('../controllers/mekanlar');
+var ctrlDigerleri= require('../controllers/digerleri');
+/* GET home page. */
+router.get('/',ctrlMekanlar.anaSayfa );
+router.get('/mekan',ctrlMekanlar.mekanBilgisi );
+router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle );
+router.get('/hakkinda',ctrlDigerleri.hakkinda );
 
 module.exports = router;
