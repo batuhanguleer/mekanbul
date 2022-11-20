f3829ca90af89449e156e14525bc0b7a2fc093e0
batuhanguleer <batuhann.guler@hotmail.com>
2022-11-20 20:50:11 +0300

    odev5

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose=require("mongoose");
 require("./mekansema")
-//var dbURI="mongodb://localhost/mekanbul";
-var dbURI="mongodb+srv://batu123:batu123@mekanbul.q9qn8ug.mongodb.net/?retryWrites=true&w=majority";
+var dbURI="mongodb://localhost/mekanbul";
+//var dbURI="mongodb+srv://batu123:batu123@mekanbul.q9qn8ug.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 function kapat(msg,callback){
     mongoose.connection.close(function(){
