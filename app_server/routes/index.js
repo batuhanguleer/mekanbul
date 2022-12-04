ad9b67d5403de9648cba3419f20db965c426d7ee
batuhanguleer <batuhann.guler@hotmail.com>
2022-12-04 22:35:29 +0300

    odev7

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,34 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMekanlar = require('../controllers/mekanlar');
+var ctrlDigerleri = require('../controllers/digerleri');
 
-const anaSayfa = function (req, res, next) {
-  res.render('anasayfa', { 'title': 'Ana sayfa' });
-}
-
-const mekanBilgisi = function (req, res, next) {
-  res.render('mekanbilgisi', { 'title': 'Mekan bilgisi' });
-}
-
-const yorumEkle = function (req, res, next) {
-  res.render('yorumekle', { 'title': 'Yorum ekle' });
-}
-
-
-module.exports = {
-
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle,
-
-}
-var express = require('express');
-var router = express.Router();
-var ctrlMekanlar= require('../controllers/mekanlar');
-var ctrlDigerleri= require('../controllers/digerleri');
-/* GET home page. */
-router.get('/',ctrlMekanlar.anaSayfa );
-router.get('/mekan',ctrlMekanlar.mekanBilgisi );
-router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle );
-router.get('/hakkinda',ctrlDigerleri.hakkinda );
-
+router.get('/',ctrlMekanlar.anaSayfa);
+router.get('/mekan/:mekanid', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni',ctrlMekanlar.yorumEkle);
+router.get('/hakkinda',ctrlDigerleri.hakkinda);
 module.exports = router;
